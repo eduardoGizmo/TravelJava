309110c65b1a338522951ecba6e5843b9fd521b0
Eduardo Gizmo <eduardogimenezmoreno@gmail.com>
2020-01-10 14:33:13 +0000

    Passenger Class done & Test Passed

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    Passenger passenger1;
+
+    @Before
+    public void setUp(){
+        passenger1 = new Passenger("Jack", 3);
+    }
+
+    @Test
+    public void shouldHavePassengerAName(){
+        assertEquals("Jack", passenger1.getPassengerName());
+    }
+
+    @Test
+    public void shouldHavePassengerNumOfBags(){
+        assertEquals(3, passenger1.getNumOfBags());
+    }
 }
